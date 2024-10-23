1ef2cfba1d2e50c4492dc4d8d233e976863c500f
kousalyapt <kousalya@sedintechnologies.com>
2024-10-23 23:24:28 +0530

    optimized and added benchmark

--- Presentation/Files_and_IO/Task/modify_csv_file.rb
+++ Presentation/Files_and_IO/Task/modify_csv_file.rb
@@ -1,58 +1,69 @@
 require 'csv'
 require 'date'
 
-@count = 0
-
-def modify_date_format_in_csv(input_file_path)
-  
+def modify_date_format_in_csv(input_file_path)  
   start_time = Time.now
   File.open(input_file_path,'r+') do |file|
-    
-  begin
-  CSV.foreach(input_file_path, headers: true) do |line|
-    
-     if @count == 0
-        file.puts line.headers.to_csv
-        @count += 1
-     end
-   
-    date = line['Date']
-    
-    if date
-      begin
-      date_obj = Date.parse(date)
-      day = date_obj.day
-
-      suffix = case day
-               when 1, 21, 31 then 'st'
-               when 2, 22 then 'nd'
-               when 3, 23 then 'rd'
-               else 'th'
-               end
-
-      formatted_date = "#{day}#{suffix} #{date_obj.strftime('%B %Y')}"
-      
-      line['Date'] = formatted_date
-      rescue ArgumentError
-        puts "Skipping invalid date format: #{date} in line #{index + 1}"
-      rescue CSV::MalformedCSVError => e
-        puts "Malformed CSV error in line #{index + 1}: #{e.message}. Skipping this line."
-      rescue => e
-        puts "An unexpected error occurred: #{e.message} in line #{index + 1}"
-      end
-    end
-
-    
-    file.puts line.to_csv
+    process_csv(file, input_file_path)     
   end
+  puts "Modified"
+  end_time = Time.now
+  puts end_time - start_time
+end
+
+def process_csv(file, input_file_path)
+  headers_written = false
+  line_number = 0
+  begin
+    CSV.foreach(input_file_path, headers: true).with_index do |line, index| 
+      line_number = index + 2   
+      write_headers(file, line,headers_written)
+      headers_written = true
+      process_line(file, line, line_number)
+    end
   rescue CSV::MalformedCSVError => e
-      puts "Malformed CSV error in line. Skipping this line."
+    puts "Malformed CSV error in line. Skipping line no #{line_number}"
   end
 end
-puts "Modified"
-end_time = Time.now
-puts end_time - start_time
 
+def write_headers(file, line, headers_written)
+  unless headers_written
+    file.puts line.headers.to_csv
+ end
+end
+
+def process_line(file, line, line_number)
+  date = line['Date']      
+    if date
+      formatted_date = format_date(date, line_number)
+      line['Date'] = formatted_date if formatted_date
+    end  
+    file.puts line.to_csv
+end
+
+def format_date(date, line_number)
+  begin
+    date_obj = Date.parse(date)
+    day = date_obj.day
+    suffix = determine_day_suffix(day)
+    formatted_date = "#{day}#{suffix} #{date_obj.strftime('%B %Y')}"
+    return formatted_date    
+  rescue ArgumentError
+    puts "Skipping invalid date format: #{date} in line #{line_number}"
+    return nil
+  rescue => e
+    puts "An unexpected error occurred: #{e.message} in line #{line_number}"
+    return nil
+  end
+end
+
+def determine_day_suffix(day)
+  case day
+  when 1, 21, 31 then 'st'
+  when 2, 22 then 'nd'
+  when 3, 23 then 'rd'
+  else 'th'
+  end
 end
 
 input_file_path = 'large_file.csv'
--- Presentation/Files_and_IO/Task/read_csv_file.rb
+++ Presentation/Files_and_IO/Task/read_csv_file.rb
@@ -1,18 +1,53 @@
+require 'benchmark'
+
 file_path = 'large_file.csv'
 
-start_time = Time.now
-File.foreach(file_path) do |row|
-  
+Benchmark.bm(20) do |x|
+
+  x.report("File.foreach") do
+    File.foreach(file_path) do |line|
+      
+    end
+  end
+
+  x.report("File.read") do
+    content = File.read(file_path)
+  end
+
+  x.report("File.readlines") do
+    lines = File.readlines(file_path) 
+  end
+
+  x.report("File.open with gets") do
+    File.open(file_path, 'r') do |file|
+      while line = file.gets
+    
+      end
+    end
+  end
+
+  x.report("File.open with readpartial") do
+    File.open(file_path, 'r') do |file|
+      begin
+        while chunk = file.readpartial(1024) 
+        
+        end
+      rescue EOFError
+        
+      end
+    end
+  end
+
+
+  x.report("IO.read") do
+    content = IO.read(file_path)
+  end
+
+
+  x.report("IO.foreach") do
+    IO.foreach(file_path) do |line|
+
+    end
+  end
+
 end
-end_time  = Time.now
-puts end_time - start_time 
-
-#1.1118672 secs
-
-
-start1_time = Time.now
-File.read(file_path)
-end1_time  = Time.now
-puts end1_time - start1_time 
-
-#0.3330874 secs
